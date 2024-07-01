4f232240344ab043d418ea955683f24adf279183
Luke Kuzmish <lakuzmish@gmail.com>
2024-07-01 13:55:02 -0400

    timing

---
+++ tests/InMemory/InMemoryTimingTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Cosmastech\StatsDClient\Tests\InMemory;
+
+use Cosmastech\StatsDClient\InMemory\InMemoryClient;
+use Cosmastech\StatsDClient\Tests\BaseTestCase;
+use Cosmastech\StatsDClient\Tests\ClockStub;
+use DateTimeImmutable;
+use PHPUnit\Framework\Attributes\Test;
+
+class InMemoryTimingTest extends BaseTestCase
+{
+    #[Test]
+    public function storesTimingRecord() {
+        // Given
+        $stubDateTime = new DateTimeImmutable("2019-02-13 07:56:00");
+        $inMemoryClient = new InMemoryClient(new ClockStub($stubDateTime));
+
+        // When
+        $inMemoryClient->timing("timing-stat", 199, 0.2, ["timing" => "some-value"]);
+
+        // Then
+        $statsRecord = $inMemoryClient->getStats();
+        self::assertCount(1, $statsRecord->timing);
+
+        $timingRecord = $statsRecord->timing[0];
+        self::assertEquals("timing-stat", $timingRecord->stat);
+        self::assertEquals(199, $timingRecord->milliseconds);
+        self::assertEquals(0.2, $timingRecord->sampleRate);
+        self::assertEqualsCanonicalizing(["timing" => "some-value"], $timingRecord->tags);
+        self::assertEquals($stubDateTime, $timingRecord->recordedAt);
+    }
+}
