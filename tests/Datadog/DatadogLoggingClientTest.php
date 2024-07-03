f38b15c5c0da1ef22fc08077b7182eadb42f7620
Luke Kuzmish <lakuzmish@gmail.com>
2024-07-03 17:16:31 -0400

    DatadogLoggingClient

---
+++ tests/Datadog/DatadogLoggingClientTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Cosmastech\StatsDClient\Tests\Datadog;
+
+use Cosmastech\StatsDClient\Tests\BaseTestCase;
+use PHPUnit\Framework\TestCase;
+
+class DatadogLoggingClientTest extends BaseTestCase
+{
+    // @todo
+}
