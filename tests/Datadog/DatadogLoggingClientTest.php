6e407efc6c14126dfbd9fffad2ab1eba75d223dd
Luke Kuzmish <lakuzmish@gmail.com>
2024-07-03 17:23:19 -0400

    test

--- tests/Datadog/DatadogLoggingClientTest.php
+++ tests/Datadog/DatadogLoggingClientTest.php
@@ -2,10 +2,52 @@
 
 namespace Cosmastech\StatsDClient\Tests\Datadog;
 
+use Cosmastech\StatsDClient\Datadog\DatadogLoggingClient;
 use Cosmastech\StatsDClient\Tests\BaseTestCase;
-use PHPUnit\Framework\TestCase;
+use Cosmastech\StatsDClient\Tests\Doubles\LoggerSpy;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use Psr\Log\LogLevel;
 
 class DatadogLoggingClientTest extends BaseTestCase
 {
+    private LoggerSpy $loggerSpy;
+
+    public function setUp(): void
+    {
+        $this->loggerSpy = new LoggerSpy();
+    }
+
+    #[Test]
+    #[DataProvider("logLevelsDataProvider")]
+    public function respectsLogLevel(string $logLevel)
+    {
+        // Given
+        $datadogLoggingClient = new DatadogLoggingClient($this->loggerSpy, $logLevel);
+
+        // When
+        $datadogLoggingClient->increment("some-stat");
+
+        // Then
+        $logs = $this->loggerSpy->getLogs();
+
+        self::assertCount(1, $logs);
+        self::assertEquals($logLevel, $logs[0]["level"]);
+    }
+
+    public static function logLevelsDataProvider(): array
+    {
+        return [
+            "debug" => [LogLevel::DEBUG],
+            "info" => [LogLevel::INFO],
+            "notice" => [LogLevel::NOTICE],
+            "warning" => [LogLevel::WARNING],
+            "error" => [LogLevel::ERROR],
+            "critical" => [LogLevel::CRITICAL],
+            "alert" => [LogLevel::ALERT],
+            "emergency" => [LogLevel::EMERGENCY],
+        ];
+    }
     // @todo
+
 }
