7794a4365ca9a70a307a5da02fb1b96844ea310a
Luke Kuzmish <lakuzmish@gmail.com>
2024-07-10 06:50:37 -0400

    datadoglogging improvements

--- tests/Clients/Datadog/DatadogLoggingClientTest.php
+++ tests/Clients/Datadog/DatadogLoggingClientTest.php
@@ -6,16 +6,20 @@ use Cosmastech\PsrLoggerSpy\LogFactory;
 use Cosmastech\PsrLoggerSpy\LoggerSpy;
 use Cosmastech\StatsDClientAdapter\Clients\Datadog\DatadogLoggingClient;
 use Cosmastech\StatsDClientAdapter\Tests\BaseTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use Psr\Log\LogLevel;
 
+#[CoversClass(DatadogLoggingClient::class)]
 class DatadogLoggingClientTest extends BaseTestCase
 {
     private LoggerSpy $loggerSpy;
 
     public function setUp(): void
     {
+        parent::setUp();
+
         $this->loggerSpy = new LoggerSpy(new LogFactory());
     }
 
@@ -24,7 +28,7 @@ class DatadogLoggingClientTest extends BaseTestCase
     public function respectsLogLevel(string $logLevel): void
     {
         // Given
-        $datadogLoggingClient = new DatadogLoggingClient($this->loggerSpy, $logLevel);
+        $datadogLoggingClient = new DatadogLoggingClient($this->loggerSpy, logLevel: $logLevel);
 
         // When
         $datadogLoggingClient->increment("some-stat");
