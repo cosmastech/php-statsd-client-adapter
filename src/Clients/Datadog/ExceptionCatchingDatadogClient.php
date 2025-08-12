edc93361d5bde7c4d61314e6a9f26a2ba48d4b06
Luke Kuzmish <lakuzmish@gmail.com>
2025-08-12 11:54:40 -0400

    safe dog

---
+++ src/Clients/Datadog/ExceptionCatchingDatadogClient.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Cosmastech\StatsDClientAdapter\Clients\Datadog;
+
+use Closure;
+use DataDog\DogStatsd;
+use Throwable;
+
+class ExceptionCatchingDatadogClient extends DogStatsd
+{
+    /**
+     * The callback to execute when there is an exception flushing stats to DataDog.
+     *
+     * @var Closure(Throwable, mixed): void
+     */
+    protected Closure $exceptionCallback;
+
+    /**
+     * @{inheritDoc}
+     *
+     * @param  (Closure(\Throwable, mixed): void)  $exceptionCallbackHandler The callback to execute when there is an exception flushing stats to DataDog
+     */
+    public function __construct(
+        array $config,
+        Closure $exceptionCallbackHandler,
+    ) {
+        parent::__construct($config);
+
+        $this->exceptionCallback = $exceptionCallbackHandler;
+    }
+
+    /**
+     * @param  mixed $message
+     * @return void
+     */
+    public function report($message)
+    {
+        try {
+            parent::report($message);
+        } catch (Throwable $exception) {
+            call_user_func($this->exceptionCallback, $exception, $message);
+        }
+    }
+}
